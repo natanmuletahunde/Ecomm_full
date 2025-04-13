3d3be178b150ea0ccff53f1b2a98b5fae9625ab1
pivnati <natanmuleta911@gmail.com>
2025-04-12 18:31:48 -0700

    create the .env file in the admin folder

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,9 +6,12 @@ import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
 import { useState } from 'react'
+import Login from './components/Login'
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 const App = () => {
 
-  const [token ,setToken] = useState('90');
+  const [token ,setToken] = useState('');
 
   return (
     <div className='bg-gray-50 min-h-screen'>
