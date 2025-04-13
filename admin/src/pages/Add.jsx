6506812ca92d7e4ed428f246a71c9bf51e2e4b8e
pivnati <natanmuleta911@gmail.com>
2025-04-13 16:02:46 -0700

    correct the list.jsx file

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { assets } from '../assets/assets';
+import axios from 'axios';
+import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
-const Add = () => {
+const Add = ({token}) => {
 
   const [image1 ,setImage1]  = useState(false);
   const [image2 ,setImage2]  = useState(false);
@@ -14,47 +17,83 @@ const Add = () => {
   const [category,setCategory] =useState('Men');
   const [subcategory,setSubCategory] =useState('TopWear');
   const [price,setPrice] =useState('');
-  const [size,setSize] =useState('');
+  const [sizes,setSizes] =useState('');
   const [bestseller,setBestSeller] =useState(false);
+  
+  const onSubmitHandler = async (e) =>{
+  e.preventDefault();
+  try {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('category', category);
+    formData.append('subcategory', subcategory);
+    formData.append('price', price);
+    formData.append('sizes', JSON.stringify(sizes));
+    formData.append('bestseller', bestseller);
 
- 
+
+    image1 && formData.append('image1', image1);  
+    image2 && formData.append('image2', image2);
+    image3 && formData.append('image3', image3);
+    image4 && formData.append('image4', image4); 
+     
+    const response = await axios.post(backendUrl + "/api/product/add" ,formData,{headers:{token}})
+    console.log(response.data)
+    if(response.data.success){
+      toast.success(response.data.message)
+      setName('')
+      setDescription('')
+      setImage1(false)
+      setImage2(false)
+      setImage3(false)
+      setImage4(false)   
+    }
+    else{
+      toast.error(response.data.message); 
+    }
+  } catch (error) {
+     console.log(error); 
+     toast.error(error.message)
+  }
+  }
+  
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <div className='flex flex-col w-full gap-3'>
         <p className='mb-2 '>Upload Image</p>
        <div className='flex gap-2'>
        <label htmlFor="image1" style={{ cursor: 'pointer' }}>
-        <img className='w-20' src={assets.upload_area} alt="Upload area" />
-        <input type="file" id="image1" hidden />
+        <img className='w-20' src={!image1 ? assets.upload_area:URL.createObjectURL(image1)}  />
+        <input onChange={(e)=>setImage1(e.target.files[0])} type="file" id="image1" hidden />
       </label>
       <label htmlFor="image2" style={{ cursor: 'pointer' }}>
-        <img className='w-20' src={assets.upload_area} alt="Upload area" />
-        <input type="file" id="image2" hidden />
+        <img className='w-20' src={!image2 ? assets.upload_area:URL.createObjectURL(image2)} />
+        <input onChange={(e)=>setImage2(e.target.files[0])} type="file" id="image2" hidden />
       </label>
       <label htmlFor="image3" style={{ cursor: 'pointer' }}>
-        <img className='w-20' src={assets.upload_area} alt="Upload area" />
-        <input type="file" id="image3" hidden />
+        <img className='w-20' src={!image3 ? assets.upload_area:URL.createObjectURL(image3)}/>
+        <input onChange={(e)=>setImage3(e.target.files[0])} type="file" id="image3" hidden />
       </label>
       <label htmlFor="image4" style={{ cursor: 'pointer' }}>
-        <img className='w-20' src={assets.upload_area} alt="Upload area" />
-        <input type="file" id="image4" hidden />
+        <img className='w-20' src={!image4 ? assets.upload_area:URL.createObjectURL(image4) } />
+        <input onChange={(e)=>setImage4(e.target.files[0])} type="file" id="image4" hidden />
       </label>
        </div>
     
       </div>
       <div className='w-full '>
         <p className='mb-2 '>Product name</p>
-        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type Here' required />
+        <input onChange={(e)=>setName(e.target.value )} value={name} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type Here' required />
       </div>
-
       <div className='w-full '>
         <p className='mb-2 '>Product description</p>
-        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write Content Here' />
+        <textarea  onChange={(e)=>setDescription(e.target.value )} value={description} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write Content Here' />
       </div>
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div className=''>
               <p className='mb-2'>Product Category</p>
-              <select className='px-3 py-2' name="" id="">
+              <select onChange={(e)=>setCategory(e.target.value)} className='px-3 py-2' name="" id="">
                 <option value="Men">Men</option>
                 <option value="Women">Women</option>
                 <option value="Kids">Kids</option>
@@ -63,7 +102,7 @@ const Add = () => {
          
         <div className=''>
               <p className='mb-2' >Sub Category</p>
-              <select className='px-3 py-2' name="" id="">
+              <select  onChange={(e)=>setSubCategory(e.target.value)} className='px-3 py-2' name="" id="">
                 <option value="TopWear">TopWear</option>
                 <option value="BottomWear">BottomWear</option>
                 <option value="WinterWear">WinterWear</option>
@@ -71,7 +110,7 @@ const Add = () => {
         </div>
         <div>
           <p className='mb-2 '>Product Price</p>
-          <input className='w-full px-3 py-2 sm:w-[120px]' placeholder='25' type="Number" name="" id=""  />
+          <input onChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' placeholder='25' type="Number" name="" id=""  />
         </div>
         
       </div>
@@ -79,23 +118,26 @@ const Add = () => {
       <div>
         <p className='mb-2'>Product Sizes </p>
         <div className='flex gap-3 '>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          <div onClick={()=>setSizes(prev=>prev.includes('S')? prev.filter(item => item !== 'S'):[...prev,'S'])}>
+            <p className={`${sizes.includes('S') ? 'bg-pink-100':'bg-slate-200'} px-3 py-1 cursor-pointer`}>S</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer' >M</p>
+          <div onClick={()=>setSizes(prev=>prev.includes('M')? prev.filter(item => item !== 'M'):[...prev,'M'])}>
+            <p className={`${sizes.includes('M') ? 'bg-pink-100':'bg-slate-200'} px-3 py-1 cursor-pointer`} >M</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          <div onClick={()=>setSizes(prev=>prev.includes('L')? prev.filter(item => item !== 'L'):[...prev,'L'])}>
+            <p className={`${sizes.includes('L') ? 'bg-pink-100':'bg-slate-200'} px-3 py-1 cursor-pointer`} >L</p>
           </div>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          <div onClick={()=>setSizes(prev=>prev.includes('XL')? prev.filter(item => item !== 'XL'):[...prev,'XL'])}>
+            <p className={`${sizes.includes('XL') ? 'bg-pink-100':'bg-slate-200'} px-3 py-1 cursor-pointer`}>XL</p>
+          </div>
+          <div onClick={()=>setSizes(prev=>prev.includes('XXL')? prev.filter(item => item !== 'XXL'):[...prev,'XXL'])}>
+            <p className={`${sizes.includes('XXL') ? 'bg-pink-100':'bg-slate-200'} px-3 py-1 cursor-pointer`}>XXL</p>
           </div>
         </div>
       </div>
 
       <div className='flex gap-2 mt-2'>
-        <input type="checkbox" name="" id="bestseller" />
+        <input onChange={()=>setBestSeller(prev=>!prev)} checked={bestseller} type="checkbox" name="" id="bestseller" />
         <label className='cursor-pointer' htmlFor="bestseller">
           Add to Best Seller
         </label>
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { backendUrl } from '../App'
 
 const List = () => {
-  return (
-    <div>List</div>
+  const [list, setList] = useState([])
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/api/product/list`)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchList()
+  }, [])
+
+  return (      
+    <div>
+     
+    </div>
   )
 }
 
