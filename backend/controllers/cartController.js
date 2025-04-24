75d790f55be95f888ade4cdfadf4847e910f3035
pivnati <natanmuleta911@gmail.com>
2025-04-24 11:08:37 -0700

    create the cart router

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+// add to cart to user 
+const addToCart = async (req,res)=>{
+
+}
+const updateCart = async (req,res)=>{
+
+}
+const getUserCart = async (req,res)=>{
+
+}
+export {addToCart,updateCart,getUserCart};
