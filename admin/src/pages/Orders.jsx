abb2d193e3086cf4b32904adb95dccd6a8cb6516
pivnati <natanmuleta911@gmail.com>
2025-04-25 18:58:25 -0700

    create the orderslist in the  and connect it with db

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState,useEffect} from 'react'
+import axios from 'axios'
+import {toast} from 'react-toastify'
+import { backendUrl } from '../App'
 
-const Orders = () => {
+
+const Orders = ({token}) => {
+const [orders , setOrders] = useState([])
+
+const  fetchAllOrders= async ()=>{
+   
+     if(!token){
+      return null
+     }
+     try {
+      const response = await axios.post(backendUrl +'/api/orders/list',{},{headers:{token}})
+
+      if(response.data.success){
+        setOrders(response.data.success)
+      }
+      else{
+         toast.error(response.error)
+      }
+     } catch (error) {
+       toast.error(error.message)
+     }
+}
+useEffect(()=>{
+  fetchAllOrders()
+})
   return (
-    <div>Orders</div>
+    <div>
+        
+
+    </div>
   )
 }
 
