d905c96761f41fc64bb2474f5eab00a1b11bebee
pivnati <natanmuleta911@gmail.com>
2025-04-26 10:52:47 -0700

    finsh the placeorder

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -53,15 +53,29 @@ const PlaceOrder = () => {
 
       switch (method) {
         case 'cod':
-          { const response = await axios.post(backendUrl + '/api/order/place', orderData, { headers: { token } })
-          if (response.data.success) {
-            setCartItems({})
-            navigate('/orders')
+          {
+            const response = await axios.post(backendUrl + '/api/order/place', orderData, { headers: { token } })
+            if (response.data.success) {
+              setCartItems({})
+              navigate('/orders')
+            }
+            else {
+              toast.error(response.data.message)
+            }
+            break;
           }
-          else {
-            toast.error(response.data.message)
+        case 'stripe':
+          {
+            const response = await axios.post(backendUrl + '/api/order/stripe', orderData, { headers: { token } })
+            if (response.data.success) {
+              const { session_url } = response.data
+              window.location.replace(session_url)
+            }
+            else {
+              toast.error(response.data.message)
+            }
+            break;
           }
-          break; }
         default:
           break;
       }
@@ -71,6 +85,7 @@ const PlaceOrder = () => {
       toast.error(error.message)
     }
   }
+
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
@@ -105,8 +120,8 @@ const PlaceOrder = () => {
           <Title text1={'PAYMENT'} text2={'METHOD'} />
           {/*------payment method */}
           <div className='flex gap-3 flex-col lg:flex-row'>
-            <div onClick={() => setMethod('strip')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'strip' ? 'bg-green-400' : ''}`}></p>
+            <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
             </div>
             <div onClick={() => setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
